2d26f8dc89dfe2391e00b842d79e6605544e4866
killzzy <martin.bartos013@gmail.com>
2020-11-04 18:53:27 +0000

    Oprava funkci

--- script.js
+++ script.js
@@ -4,18 +4,16 @@ function vysl() {
   let c = document.getElementById("cislo3").valueAsNumber;
 
   let d = (b*b)-(4*a*c);
+  document.getElementById("Diskriminant").innerHTML=d;
 
   if (d < 0){
-    document.getElementById("Diskriminant").innerHTML=d;
     document.getElementById("x1").innerHTML="Rovnice nemá řešení";
     document.getElementById("x2").innerHTML="Rovnice nemá řešení";
   } else if (d == 0){
-    document.getElementById("Diskriminant").innerHTML=d;
     x1 = -b/(2*a)
     document.getElementById("x1").innerHTML=x1;
     document.getElementById("x2").innerHTML=x1;
   } else {
-    document.getElementById("Diskriminant").innerHTML=d;
     x1 = (-b + Math.sqrt(d))/(2*a)
     x2 = (-b - Math.sqrt(d))/(2*a)
     document.getElementById("x1").innerHTML=x1;
@@ -44,19 +42,17 @@ function prvocisla(){
   let num = 1;
 
   while (count < pocet) {
+  num = num + 1;
     if (P(num)) {
       vypis = vypis + num + " ";
-      num = num + 1;
       count = count + 1;
-    } else {
-      num = num + 1;
     }
   }
   document.getElementById("vypis").innerHTML = vypis;
 }
 
 function P(c) {
-  for (let d=2; d<Math.sqrt(c); d++){
+  for (let d=2; d<=Math.sqrt(c); d++){
     if(c % d ==0) {
       return false;
     }
